ea4b0a44472b084de694fb5b44a887d156a1a134
J <jpham26@gmail.com>
2024-10-21 15:16:17 -0700

    Add router for adjustItemQuantity and updateItemPrice

--- backend/routes/itemsRouter.js
+++ backend/routes/itemsRouter.js
@@ -8,6 +8,8 @@ router.get('/:id', itemController.getItemById);
 router.get('/category/:id', itemController.getItemsByCategoryId);
 router.post('/', itemController.createItem);
 router.put('/:id', itemController.updateItem);
+router.patch('/:id/quantity', itemController.adjustItemQuantity);
+router.patch('/:id/price', itemController.updateItemPrice);
 router.delete('/:id', itemController.deleteItem);
 
 module.exports = router;
