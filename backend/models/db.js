79b879e6e807f225f2757f22f4320d43739cd65e
J <jpham26@gmail.com>
2024-10-25 15:43:15 -0700

    Add code to parse DECIMAL as floats

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,6 +1,8 @@
 // models/db.js
-const { Pool } = require('pg');
+const { Pool, types } = require('pg');
 require('dotenv').config();
+
+types.setTypeParser(types.builtins.NUMERIC, value => parseFloat(value));
 
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL
